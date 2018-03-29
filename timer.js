d15dde64a6fc2dab4f94fbd843f9b0046483c482
Kenil Vora <kvora@ucsd.edu>
2018-03-28 23:46:38 -0700

    Added basic functionality

--- timer.js
+++ timer.js
@@ -1,32 +1,53 @@
 
+function startTimer(days, hours, minutes, seconds)
+{
+	var remaining = setInterval( function updateSeconds() 
+	{
+		if (seconds > 1)
+		{
+			seconds--;
+			document.getElementById("seconds").value = seconds;
+		}
+		else
+		{
+			clearInterval(remaining);
+			console.log("ENTERED ELSE CASE");
+			document.getElementById("timeup").style.display = "inline-block";
+		}
+
+		console.log(seconds + "STILL IN LOOP");
+
+	}, 1000);
+
+	console.log("NOT IN LOOP");
+}
+
 function storeInputs() {
 	
 	// get inputs from each field
 	var days = document.getElementById("days").value;
-	if (isNaN(days)) { days = 0; }
+	if (isNaN(days)) {
+		days = 0;
+		document.getElementById("days").value = days;
+	}
 
 	var hours = document.getElementById("hours").value;
-	if (isNaN(hours)) { hours = 0; }
+	if (isNaN(hours)) { 
+		hours = 0;
+		document.getElementById("hours").value = hours;
+	}
 
 	var minutes = document.getElementById("minutes").value;
-	if (isNaN(minutes)) { minutes = 0; }
+	if (isNaN(minutes)) {
+		minutes = 0;
+		document.getElementById("minutes").value = minutes;
+	}
 
 	var seconds = document.getElementById("seconds").value;
-	if (isNaN(seconds)) { seconds = 0; }
+	if (isNaN(seconds)) { 
+		seconds = 0; 
+		document.getElementById("seconds").value = seconds;
+	}
 
-	// hide input fields
-	document.getElementById("inputs").style.display = "none";
-	var nodes = document.getElementById("clock").childNodes;
-
-	for(var i=0; i<nodes.length; i++) {
-	    if (nodes[i].nodeName.toLowerCase() == 'span') {
-	         nodes[i].style.display = "inline-block";
-	     }
-}
-
-	// call function to set values 
-
-
-	// show clock countdown
-
+	startTimer(days, hours, minutes, seconds);
 }
